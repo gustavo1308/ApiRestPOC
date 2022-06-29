e33d3ebb38e3fe123b5888ffd73f23e7c8f1c9fd
gustavo arjona <gustavoarjona3@gmail.com>
2022-06-28 21:52:23 -0300

    Criando domain com cliente, adicionando referencias ao entity framework e mapeando domain como dbset

---
+++ AR.Pasta/ContextoPrincipal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using AR.Domain;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace AR.Pasta
+{
+    public class ContextoPrincipal : Dbcontext
+
+    {
+        public ContextoPrincipal(DbContextOptions options) 
+            : base()
+        {
+        }
+
+        public DbSet<Cliente>
+    }
+}
---
+++ AR.Pasta/Dbcontext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AR.Pasta
+{
+    public class Dbcontext
+    {
+        private DbContextOptions options;
+
+        public Dbcontext(DbContextOptions options)
+        {
+            this.options = options;
+        }
+    }
+}
